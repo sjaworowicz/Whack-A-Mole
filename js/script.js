a716d40c2471cdf62af9e1e36e921aead9d8b587
sjaworowicz <125sjaworowicz@frhsd.com>
2024-01-10 09:55:18 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,72 @@
+let currMoleTile;
+let currPlantTile;
+let score = 0;
+let gameOver = false;
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    for (let i = 0; i <9; i++) {
+
+        let tile = document.createElement("div")
+        tile.id = i.toString();
+        tile.addEventListener("click", selectTile);
+        document.getElementById("board").appendChild(tile);
+    }
+
+    setInterval(setMole, 2000);
+    setInterval(setPlant, 3000);
+
+}
+
+function getRandomTile() {
+    let num = Math.floor(Math.random()* 9)
+    return num.toString();
+}
+
+function setMole() {
+
+    if(currMoleTile) {
+        currMoleTile.innerHTML ="";
+    }
+
+    let mole = document.createElement("img");
+    mole.src = "./img/MontyMole.webp";
+
+    let num = getRandomTile();
+    if(currPlantTile &&currPlantTile.id ==num) {
+        return;
+    }
+    currMoleTile = document.getElementById(num);
+    currMoleTile.appendChild(mole);
+}
+
+function setPlant() {
+    
+    if(currPlantTile) {
+        currPlantTile.innerHTML = "";
+    }
+
+    let plant = document.createElement("img");
+    plant.src = "./img/Piranha_Plant_Mario.webp";
+
+    let num = getRandomTile();
+    if(currMoleTile && currMoleTile.id == num) {
+        return;
+    }
+    currPlantTile = document.getElementById(num);
+    currPlantTile.appendChild(plant);
+}
+
+function selectTile() {
+
+    if(this == currMoleTile) {
+        score += 10;
+        document.getElementById("score").innerText = score.toString();
+    }
+    else if (this == currPlantTile) {
+        document.getElementById("score").innerText = "GAME OVER:" + score.toString();
+    }
+}
